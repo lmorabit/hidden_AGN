e3e18b09a2de5cdf916c92e06c6f92335d8080f5
lmorabit <lmorabit@gmail.com>
2024-01-30 17:10:40 +0000

    added lofar data

--- src/scripts/helper_functions.py
+++ src/scripts/helper_functions.py
@@ -4,6 +4,15 @@ from astropy.table import Table
 import astropy.units as u
 from progress.bar import Bar
 import paths
+from astropy.stats import median_absolute_deviation as apy_mad
+
+def radio_power( obs_flux_Jy, redshift, spectral_index=-0.8):
+    flux_cgs = obs_flux_Jy * 1e-23
+    DL = cosmo.luminosity_distance(redshift).value * 3.086e24 ## convert to cm
+    power_cgs = 4. * np.pi * np.power(DL,2.) * flux_cgs / np.power( 1+redshift, 1.+spectral_index )
+    ## convert to W/Hz
+    power = power_cgs * 1e-7
+    return( power )
 
 def RLF_calculate_zmax( flux_limit, source_id, fluxes, flux_errors, redshifts, spectral_indices, outfile='' ):
 
@@ -91,6 +100,94 @@ def RLF_calculate_zmax( flux_limit, source_id, fluxes, flux_errors, redshifts, s
 
     outfits = outfile + '.fits'
 
-    t.write( paths.static / outfits , format='fits', overwrite=True )
+    t.write( paths.data / outfits , format='fits', overwrite=True )
     return( t )
+
+def RLF_from_zmax( Lum, zz, lum_bins, redshift_bins, myzmax, area_cov, area_units='deg2', error_type='rms' ):
+    ## error_type can be "data" or "rms"
+    ## "data" will use measured uncertainties to calculate upper and lower values and propagate the errors
+    ## "rms" will use the estimator from Marshall 1985 and is equal to
+    ## 1/(delta log L) * sqrt( sum( 1/vmax^2 )
+
+    ## get log10 of the values
+    log10_L_total = np.log10( Lum )
+
+    ## create an array for the RLF
+    lum_func = np.zeros( (len(lum_bins),len(redshift_bins)) )
+    lum_func_up = np.copy(lum_func)
+    lum_func_lo = np.copy(lum_func)
+    ## median luminosities
+    med_lum = np.copy( lum_func )
+    med_lum_err = np.copy( lum_func )
+    ## number of objects
+    N_obj = np.copy( lum_func )
+    
+    if area_units == 'deg2':
+        ## area is in sqare degrees, convert to sr
+        area_sr = area_cov * np.power( ( np.pi / 180. ), 2. )
+        area_val = area_sr / ( 4. * np. pi )
+        ## to avoid evaluating if statements every loop
+        area_scaling = np.repeat( area_val, len(redshift_bins) )
+        area_scaling_zmax = np.repeat( 1., len(Lum))
+    elif area_units == 'Mpc2':
+        ## area in Mpc^2
+        ## this is redshift invariant because H_0 is invariant
+        angular_distance = cosmo.angular_diameter_distance( redshift_bins )
+        area_scaling = area_cov / ( 4. * np.pi * np.power( angular_distance, 2. ) )
+        angular_distance_zmax = cosmo.agular_diameter_distance( myzmax['z_max'] )
+        area_scaling_zmax = area_cov / ( 4. * np.pi * np.power( angular_distance_zmax, 2. ) )
+
+    pb = Bar('Processing', max=len(lum_bins)*len(redshift_bins))
+    for rr in np.arange( 1,len(lum_bins) ):
+        for ss in np.arange( 1, len(redshift_bins) ):
+            ## volumes for the redshift bin
+            vmin_z = cosmo.comoving_volume( redshift_bins[ss-1] ) * area_scaling[ss-1]
+            vmax_z = cosmo.comoving_volume( redshift_bins[ss] ) * area_scaling[ss]
+            lum_idx = np.where( np.logical_and( log10_L_total >= lum_bins[rr-1], log10_L_total < lum_bins[rr] ) )
+            redshift_idx = np.where( np.logical_and( zz >= redshift_bins[ss-1], zz < redshift_bins[ss] ) )
+            bin_idx = np.intersect1d( lum_idx, redshift_idx )
+            N_obj[rr-1,ss-1] = len(bin_idx)
+            if len(bin_idx) > 0:
+                vmax = cosmo.comoving_volume( myzmax['z_max'][bin_idx] )
+                greater_vmax = np.where( vmax > vmax_z )
+                vmax[greater_vmax] = vmax_z
+                vmax = vmax - vmin_z
+                ## sum 1/vmax to get lum func
+                lum_func[rr,ss] = np.log10( np.nansum( 1./vmax.value ) / ( lum_bins[rr] - lum_bins[rr-1] ) )
+                ## now ... error propagation
+                if error_type == 'data':
+                    ## intuitively this seems better, but then I end up with such small errors that vmax_up = vmax or vmax_lo = vmax
+                    vmax_up = cosmo.comoving_volume( myzmax['z_max_up'][ss-1] ) * area_scaling[ss-1]
+                    vmax_up = vmax_up - vmin_z
+                    greater_vmax = np.where( vmax_up > vmax_z )
+                    vmax_up[greater_vmax] = vmax_up[greater_vmax] - vmax_z
+                    vmax_lo = cosmo.comoving_volume( myzmax['z_max_lo'][ss-1] ) * area_scaling[ss-1]
+                    vmax_lo = vmax_lo - vmin_z
+                    greater_vmax = np.where( vmax_lo > vmax_z )
+                    vmax_lo[greater_vmax] = vmax_lo[greater_vmax] - vmax_z
+                    lum_func_up[rr,ss] = np.log10( np.nansum( 1./vmax_up.value ) )
+                    lum_func_lo[rr,ss] = np.log10( np.nansum( 1./vmax_lo.value ) )
+                elif error_type == 'rms':
+                    err_term = np.sqrt( np.nansum( 1. / np.power(vmax.value, 2.) ) ) / ( lum_bins[rr] - lum_bins[rr-1] )
+                    lum_func_up[rr,ss] = np.log10( np.power( 10., lum_func[rr,ss] ) + err_term )
+                    test = np.power( 10., lum_func[rr,ss] ) - err_term
+                    if test == 0:
+                        ## set to zero
+                        lum_func_lo[rr,ss] = 0.
+                    else:
+                        lum_func_lo[rr,ss] = np.log10( np.power( 10., lum_func[rr,ss] ) - err_term )
+                med_lum[rr,ss] = np.nanmedian( np.log10(Lum[bin_idx]) )
+                med_lum_err[rr,ss] = apy_mad( Lum[bin_idx] )
+            else:
+                print( 'Skipping empty bin.' )
+                ## set to zero
+                lum_func[rr,ss] = 0
+                lum_func_up[rr,ss] = 0
+                lum_func_lo[rr,ss] = 0
+                med_lum[rr,ss] = 0
+                med_lum_err[rr,ss] = 0
+            pb.next()
+    pb.finish()
+
+    return( lum_func, lum_func_up, lum_func_lo, med_lum, med_lum_err, N_obj )
 
--- src/scripts/mauchsadler.py
+++ src/scripts/mauchsadler.py
@@ -22,17 +22,11 @@ flux_cut = 150e-6 ## 150 uJy
 valid = np.where( np.logical_or(lotss['Total_flux'] > flux_cut, lotss['Peak_flux'] > flux_cut) )[0]
 lotss = lotss[valid]
 ## remove things with no redshifts
-
 ## for testing
 #good_z = np.where( np.isfinite(lotss['Z_BEST']) )[0]
+zmax = 0.5
 good_z = np.where(lotss['Z_BEST'] < 0.5 )[0]
 lotss = lotss[good_z]
-
-## get z-max
-lotss_zmax = RLF_calculate_zmax( flux_cut, lotss['Source_Name'], lotss['Total_flux'], lotss['E_Total_flux'], lotss['Z_BEST'], np.repeat(si,len(lotss)), outfile='zmaxes' )
-
-
-
 
 ## Total area
 rms_image = fits.open( paths.static / 'lockman_rms_starmask_optical.fits' )
@@ -43,11 +37,23 @@ pixscale = np.abs(rms_image[0].header['CDELT1'])
 pixarea = np.power(pixscale, 2.)
 area_deg2 = npix_total * pixarea
 
+## get z-max
+lotss_zmax = RLF_calculate_zmax( flux_cut, lotss['Source_Name'], lotss['Total_flux'], lotss['E_Total_flux'], lotss['Z_BEST'], np.repeat(si,len(lotss)), outfile='zmaxes' )
+
+## calculate RLFs
+redshift_bins = np.arange( 0.0, zmax+0.001,0.5 )
+lum_bins = np.arange( 19.8, 27, 0.4 ) + np.log10( np.power( (144./1400.), si ) )
+Lrad = radio_power( lotss['Total_flux'], lotss['Z_BEST'], spectral_index=si )
+RLF, RLF_up, RLF_lo, Lmed, Lmed_err, N_obj = RLF_from_zmax( Lrad, lotss['Z_BEST'], lum_bins, redshift_bins, lotss_zmax, area_deg2, area_units='deg2', error_type='rms' )
+
+
 fig = plt.figure( figsize=(5,5) )
 ## plot
 plt.plot( ms_144MHz, mauch_sadler['log10RLF_all'], color='black', linewidth=2.5, label='All' )
 plt.plot( ms_144MHz, mauch_sadler['log10RLF_SF'], color='magenta', linewidth=2.5, label='SFG' )
 plt.plot( ms_144MHz, mauch_sadler['log10RLF_RLAGN'], color='orange', linewidth=2.5, label='AGN' )
+## plot the lofar data
+plt.plot( Lmed, RLF[:,1], color='purple', label='data' )
 plt.xlim((20,28))
 plt.ylim(-7.5,-2)
 plt.xlabel('log('+r'$L_{\mathrm{144 MHz}}$'+' W Hz'+r'$^{-1}$'+'])')

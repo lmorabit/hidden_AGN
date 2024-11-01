75f2bd5720af80e1b59cf4dd47dfd2fe95cf1957
Leah Morabito <lmorabit@gmail.com>
2024-11-01 08:32:23 +0000

    typo

--- src/scripts/comparison_plot.py
+++ src/scripts/comparison_plot.py
@@ -43,7 +43,7 @@ lockman_tmp = lockman_vmaxes[keep_cols]
 elais_tmp = elais_vmaxes[keep_cols]
 vmaxes = vstack([lockman_tmp,elais_tmp])
 
-t = Table.read( paths.data / 'rlfs/rlfs_zmin0.003_zmax0.3_lmin20.5_lmax27_dl0.3.fits', fomrat='fits' )
+t = Table.read( paths.data / 'rlfs/rlfs_zmin0.003_zmax0.3_lmin20.5_lmax27_dl0.3.fits', format='fits' )
 
 lum_bins = np.arange( lmin, lmax, dl )
 lum_func = t['lum_func']
